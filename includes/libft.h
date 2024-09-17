4971ea63dacdd8338c0769141938f1d93208f26e
Sergio Filipe Andre Aires <sandre-a@c4r2s2.42luxembourg.lu>
2024-09-17 17:54:55 +0200

    header update

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: sandre-a <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 15:31:19 by sandre-a          #+#    #+#             */
-/*   Updated: 2024/09/12 10:13:12 by sandre-a         ###   ########.fr       */
+/*   Updated: 2024/09/17 17:54:27 by sandre-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,5 +74,6 @@ void				free_ft_split(char **strings);
 */
 char				*ft_strpbrk(const char *s, const char *c);
 char				*ft_strndup(const char *s, size_t n);
+char				*remove_first_char(char *str);
 
 #endif
