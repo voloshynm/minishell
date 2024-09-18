6d5cebdb6c9960366730aadc0e4ca40c785db230
Sergio Filipe Andre Aires <sandre-a@c4r2s3.42luxembourg.lu>
2024-09-18 22:23:29 +0200

    header updates

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mvoloshy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: sandre-a <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 16:29:13 by sandre-a          #+#    #+#             */
-/*   Updated: 2024/09/18 17:24:48 by mvoloshy         ###   ########.fr       */
+/*   Updated: 2024/09/18 22:19:51 by sandre-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,21 @@
 # define MINISHELL_H
 
 # include "color.h"
-# include "executor.h"
 # include "lexer.h"
 # include "libft.h"
 # include "parser.h"
+# include <stdio.h>
 # include <dirent.h>
+# include <fcntl.h> // For open(), O_CREAT, etc.
 # include <limits.h>
 # include <readline/history.h>
 # include <readline/readline.h>
 # include <signal.h>
 # include <stdbool.h>
-# include <termios.h>
-# include <fcntl.h>  // For open(), O_CREAT, etc.
-# include <unistd.h> // For read(), write(), close(), unlink()
-# include <stdio.h>  // For perror(), printf()
 # include <stdlib.h> // For malloc(), free()
 # include <string.h> // For strcmp(), strlen()
+# include <termios.h>
+# include <unistd.h> // For read(), write(), close(), unlink()
 
 # define PROMPT "\033[94mminishell\033[1;31m>\033[0m"
 
@@ -42,15 +41,17 @@
 
 typedef struct s_shell
 {
-	char		*input;
-	t_lexer		*lexer;
-	t_list		*parser;
-	t_exec		*exec;
-	pid_t		pid;
-	int			ex_status;
-}				t_shell;
+	char	*input;
+	t_lexer	*lexer;
+	t_list	*parser;
+	pid_t	pid;
+	char	**envp;
+	char	*pwd;
+	char	*oldpwd;
+	int		ex_status;
+}			t_shell;
 
-enum			e_err_state
+enum		e_err_state
 {
 	OK = 0,
 	ALLOC_FAILURE = 1,
@@ -66,9 +67,12 @@ enum			e_err_state
 	DUP2_ERR = 11
 };
 
-void			prompt_loop(t_shell *m);
+void		prompt_loop(t_shell *m);
 
-int				p_error(int err_id, void *arg);
-int				input_error(char *input);
+int			p_error(int err_id, void *arg);
+int			input_error(char *input);
+
+//int			excecute(t_exec *exec);
+int			command_exists(t_shell *m);
 
 #endif
