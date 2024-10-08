994579de5f7d3fcbc6b68d1c9910690ce4fcb96e
Maksym Voloshyn <mvoloshy@c3r8s5.42luxembourg.lu>
2024-10-08 19:38:14 +0200

    fixed echo finally

--- sources/builtins/echo.c
+++ sources/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: mvoloshy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 16:45:16 by mvoloshy          #+#    #+#             */
-/*   Updated: 2024/10/08 19:36:23 by mvoloshy         ###   ########.fr       */
+/*   Updated: 2024/10/08 19:37:55 by mvoloshy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,11 @@ int	echo(char **cmd)
 
 	print_new_line = 1;
 	i = 0;
-	while (cmd[++i] && strcmp(cmd[i], "-n") == 0)
+	while (cmd[++i] && ft_strcmp(cmd[i], "-n") == 0)
 		print_new_line = 0;
 	while (cmd[i])
 	{
-		write(STDOUT_FILENO, cmd[i], strlen(cmd[i]));
+		write(STDOUT_FILENO, cmd[i], ft_strlen(cmd[i]));
 		if (cmd[++i])
 			write(STDOUT_FILENO, " ", 1);
 	}
