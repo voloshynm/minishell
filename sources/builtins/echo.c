99769c0420afd897d70ff3958dbfc7622e2ea8d9
Maksym Voloshyn <mvoloshy@c1r5s5.42luxembourg.lu>
2024-10-08 12:24:49 +0200

    fixed echo

--- sources/builtins/echo.c
+++ sources/builtins/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sandre-a <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mvoloshy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 16:13:26 by eyasa             #+#    #+#             */
-/*   Updated: 2024/10/04 17:26:33 by sandre-a         ###   ########.fr       */
+/*   Updated: 2024/10/08 12:21:48 by mvoloshy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,14 @@
 /*
 ^ bool is initialized to false, we invert the logic to print or not \n
 */
-int	echo(char **arg)
+int	echo(char **cmd)
 {
 	int		i;
 	bool	newline;
 	int		j;
-	char	**cmd;
 
-	cmd = arg;
 	i = 1;
+	newline = false;
 	while (cmd[i] && cmd[i][0] == '-' && cmd[i][1] == 'n')
 	{
 		j = 1;
